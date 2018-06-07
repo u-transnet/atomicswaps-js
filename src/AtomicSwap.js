8a6942b7819cb2f6848d79493ec7caecae2c521c
Ilya Shmelev <ishmelev23@gmail.com>
2018-06-07 16:49:59 +0300

    Switch to object based structure

--- src/AtomicSwap.js
+++ src/AtomicSwap.js
@@ -13,54 +13,64 @@ class NotImplementedError extends Error{
 
 class AtomicSwap{
 
-    constructor(initiatorAddress, privateKey){
+    /**
+     *
+     * @param {string} privateKey - privateKey for signing transactions
+     * @param {string} secretHash - hash of secret string (ripemd160(hash256(secret)))
+     * @param {string} from - owner of funds
+     * @param {string} to - address of the participant
+     * @param {int} refundTime - time remaining to refund will be available
+     * @param {float} amount - amount of tokens to transfer
+     * @param {string=} asset - tokens to transfer
+     */
+    constructor(privateKey, from, to, secretHash, refundTime,  amount, asset){
         if(this.constructor === AtomicSwap)
             throw Error("You can't instantiate AtomicSwap class because it's abstract");
 
-        this.initiatorAddress = initiatorAddress;
         this.privateKey = privateKey;
+        this.from = from;
+        this.to = to;
+        this.amount = amount;
+        this.asset = asset;
+        this.secretHash = secretHash;
+        this.refundTime = refundTime;
     }
 
     /**
      * Initiate atomic swap
-     * @param {int} refundTime - time remaining to refund will be available
-     * @param {string} secretHash - hash of secret string (ripemd160(hash256(secret)))
-     * @param {string} address - address of the participant
-     * @param {float} amount - amount of tokens to transfer
-     * @param {string=} asset - tokens to transfer
      */
-    initiate(refundTime, secretHash, address, amount, asset){
+    initiate(){
         throw NotImplementedError();
     }
 
 
     /**
      * Participate to atomic swap
-     * @param {int} refundTime - time remaining to refund will be available
-     * @param {string} secretHash - hash of secret string (ripemd160(hash256(secret)))
-     * @param {string} address - address of the participant
-     * @param {float} amount - amount of tokens to transfer
-     * @param {string=} asset - tokens to transfer
      */
-    participate(refundTime, secretHash, address, amount, asset){
+    participate(){
         throw NotImplementedError();
     }
 
     /**
      * Redeem funds with given secret
      * @param {string} secret - secret string used for swap initiating
-     * @param {string} from  - address of swap transaction initiator
      */
-    redeem(secret, from){
+    redeem(secret){
         throw NotImplementedError();
     }
 
     /**
      * Refund funds of swap to transactions' initiator
-     * @param secretHash - hash of secret string (ripemd160(hash256(secret)))
-     * @param to - address of the swap transaction participant
      */
-    refund(secretHash, to){
+    refund(){
+        throw NotImplementedError();
+    }
+
+    /**
+     * Fetch swap based on transaction hash or parameters of swap
+     * @param {string=} transactionHash - transaction hash of the operation on swap
+     */
+    fetchSwap(transactionHash){
         throw NotImplementedError();
     }
 
